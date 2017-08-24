fc42d462633a4d225f4b62210d82e750b4531785
Sihan Li <lisihan969@gmail.com>
2017-08-23 18:40:45 -0700

    Add mplementation.

---
+++ app.js
@@ -0,0 +1,13 @@
+let httpProxy = require('http-proxy');
+
+let proxy = httpProxy.createProxyServer({
+  target: 'https://api.swiftybeaver.com',
+  changeOrigin: true,
+  secure: false
+});
+
+proxy.listen(80);
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('Authorization', req.headers.authorization);
+});
